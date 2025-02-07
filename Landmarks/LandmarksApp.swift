7a3781391fa8dca1c00ec0d6ef64d8f0ccd03130
Jeremy Puchta <hello@jeremypuchta.com>
2025-02-07 18:29:53 +0100

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Jeremy Puchta on 07.02.25.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
